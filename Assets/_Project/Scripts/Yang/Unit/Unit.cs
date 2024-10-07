c611a139c13b21f3b0298a28d377af0a437fc515
tkdenddlquf@naver.com <tkdenddlquf@naver.com>
2024-10-07 15:58:17 +0900

    클래스 명칭 변경 및 Unit 이동 개선

--- Assets/_Project/Scripts/Yang/Unit/Unit.cs
+++ Assets/_Project/Scripts/Yang/Unit/Unit.cs
@@ -12,14 +12,15 @@ namespace Yang
 
         public Unit target;
 
-        private bool hit;
+        private bool onEllipse;
+        private Unit onFllipseTarget;
         private Vector3 movePos;
 
         private float checkDist;
         private float checkClosetDist;
 
         private Animator animator;
-        private PathFinding pathFinding;
+        private EllipseCollider pathFinding;
 
         private UnitManager manager;
 
@@ -31,7 +32,7 @@ namespace Yang
             TryGetComponent(out pathFinding);
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (manager == null) return;
 
@@ -91,43 +92,21 @@ namespace Yang
 
             animator.Play("Walk");
 
-            hit = false;
-            movePos = Vector3.zero;
-
-            if (pathFinding.OnAttackEllipse(transform.position, target.pathFinding) <= 1)
+            if (onEllipse)
             {
-                if (pathFinding.OnHitEllipse(transform.position, target.pathFinding) <= 1)
-                {
-                    movePos = pathFinding.AroundTarget(target.pathFinding);
-
-                    state = UnitState.Move;
-                    hit = true;
-                }
-                else state = UnitState.Attack;
+                if (pathFinding.OnHitEllipse(transform.position, onFllipseTarget.pathFinding) > 1.1f) onEllipse = false;
             }
             else
             {
-                for (int i = 0; i < manager.units.Count; i++)
-                {
-                    if (manager.units[i] != this && pathFinding.OnHitEllipse(transform.position, manager.units[i].pathFinding) <= 1)
-                    {
-                        movePos += pathFinding.AroundTarget(manager.units[i].pathFinding);
+                onEllipse = OnEllipse();
 
-                        state = UnitState.Move;
-                        hit = true;
-                    }
-                }
+                if (pathFinding.OnAttackEllipse(transform.position, target.pathFinding) <= 1) state = UnitState.Attack;
 
-                if (movePos == Vector3.zero) movePos = moveSpeed * Time.deltaTime * (target.transform.position - transform.position).normalized;
-            }
-
-            transform.position += movePos;
-
-            if (!hit)
-            {
                 if (movePos.x < 0) transform.localScale = new Vector3(1, 1, 1);
                 else if (movePos.x > 0) transform.localScale = new Vector3(-1, 1, 1);
             }
+
+            transform.position = Vector2.MoveTowards(transform.position, transform.position + movePos, moveSpeed);
         }
 
         private void Attack()
@@ -141,19 +120,72 @@ namespace Yang
 
             animator.Play("Attack");
 
-            if (pathFinding.OnHitEllipse(transform.position, target.pathFinding) <= 1)
+            if (OnEllipse())
             {
                 state = UnitState.Move;
 
                 return;
             }
 
-            if (pathFinding.OnAttackEllipse(transform.position, target.pathFinding) <= 1) Debug.Log("Attack the target!");
-            else state = UnitState.Move;
+            if (pathFinding.OnAttackEllipse(transform.position, target.pathFinding) > 1)
+            {
+                state = UnitState.Move;
+
+                return;
+            }
+
+            Debug.Log("Attack the target!");
 
             if (target.transform.position.x < transform.position.x) transform.localScale = new Vector3(1, 1, 1);
             else if (target.transform.position.x > transform.position.x) transform.localScale = new Vector3(-1, 1, 1);
         }
+
+        private bool OnEllipse()
+        {
+            movePos = moveSpeed * (target.transform.position - transform.position).normalized;
+
+            for (int i = 0; i < manager.units.Count; i++)
+            {
+                if (manager.units[i] != this && pathFinding.OnHitEllipse(transform.position + movePos, manager.units[i].pathFinding) <= 1)
+                {
+                    onFllipseTarget = manager.units[i];
+                    movePos = pathFinding.AroundTarget(manager.units[i].pathFinding, movePos);
+
+                    state = UnitState.Move;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.gray;
+
+            Vector2 endPoint = transform.position + movePos.normalized * 5;
+
+            Gizmos.DrawLine(transform.position, endPoint);
+
+            DrawArrow(endPoint, movePos.normalized);
+        }
+
+        private void DrawArrow(Vector2 position, Vector2 direction)
+        {
+            // 화살표의 크기
+            float arrowSize = 0.2f;
+
+            // 화살표 끝 좌표
+            Vector2 right = position + direction * arrowSize;
+            Vector2 left = position - direction * arrowSize;
+
+            // 화살표 머리 부분을 그리기 위한 기즈모 선
+            Gizmos.DrawLine(position, right);
+            Gizmos.DrawLine(position, left);
+        }
+#endif
     }
 
     public enum UnitState
