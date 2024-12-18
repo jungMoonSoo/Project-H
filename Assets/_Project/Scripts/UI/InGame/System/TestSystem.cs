d3491b84f8a9550e7a910be4a4999ce7146ba84d
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-18 20:45:46 +0900

    기존의 StandardCoreManager 수정
    TestSystem & StandardCoreManager로 나누었습니다.

---
+++ Assets/_Project/Scripts/UI/InGame/System/TestSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSystem : Singleton<TestSystem>
+{
+    //타이머 관련 함수
+    float timeInSeconds = 120f;     //초기 타이머 시간(한판에 걸리는 시간)
+    bool isTimerRunning = false;    //타이머 실행 여부
+    private void Update()
+    {
+        if (isTimerRunning) //타이머 관련 Update
+        {
+            timeInSeconds -= Time.deltaTime;
+            StandardCoreManager.Instance.UpdateTimerText(timeInSeconds);
+            // 시간이 0 이하가 되면 타이머 정지 => 패배 문구
+            if (timeInSeconds <= 0)
+            {
+                timeInSeconds = 0;
+                StandardCoreManager.Instance.GameDefeat();
+                StopTimer();
+            }
+        }
+    }
+
+    public void StartTimer()// 타이머 시작
+    {
+        timeInSeconds += Time.deltaTime;
+        isTimerRunning = true;
+    }
+    public void StopTimer()//타이머 멈추기
+    {
+        isTimerRunning = false;
+    }
+}
