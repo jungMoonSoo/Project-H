32b1c9c455c56406c4e550bec03fa7b943f78a09
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-19 12:49:15 +0900

    경고 문구 출력 및 Unit 생성 알고리즘 수정

--- Assets/_Project/Scripts/UI/InGame/UnitDeploymentManager.cs
+++ Assets/_Project/Scripts/UI/InGame/UnitDeploymentManager.cs
@@ -9,8 +9,10 @@ public class UnitDeploymentManager : Singleton<UnitDeploymentManager>
     [Header("GameObject 연결")]
     [SerializeField] GameObject unitDeploymentManger;
     [SerializeField] GameObject StandardCoreFieldUi;
+    [SerializeField] GameObject warnningObject;
     [SerializeField] Text stageNumber;
     [SerializeField] Text unitNumber;
+
     [Header("TEST")]
     [SerializeField] GameObject managers;
 
@@ -28,7 +30,7 @@ public class UnitDeploymentManager : Singleton<UnitDeploymentManager>
     }
     public void BackWindowButton() //뒤로 가기 버튼
     {
-        Debug.Log("[Unit Deployment Manager]  뒤로가기 버튼을 눌렸습니다. ");
+        Debug.Log("[Unit Deployment Manager]뒤로가기 버튼을 눌렸습니다. ");
     }
 
     public void StageTextUpdate(int front, int back)
@@ -47,7 +49,7 @@ public class UnitDeploymentManager : Singleton<UnitDeploymentManager>
 
     public void GameEntrance()
     {
-        Debug.Log("[Unit Deployment Manager]  게임에 입장하셨습니다. ");
+        Debug.Log("[Unit Deployment Manager]게임에 입장하셨습니다. ");
         unitDeploymentManger.SetActive(false);
         StandardCoreFieldUi.SetActive(true);
     }
@@ -55,36 +57,50 @@ public class UnitDeploymentManager : Singleton<UnitDeploymentManager>
     //Unidad Manager에서 Unidad Statuses 배열 오류  ==> 1이상 부터는 인식을 못함 
     public void BlocksmithButton() //대장장이 버튼 
     {
-        managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(0), 0, true);
-        currentUnitNumber++;
-        UnitDeployTextUpdate(currentUnitNumber);
+        UnitCreat(0);
     }
     
     public void AlchemyButton()//물약이 버튼
     {
-        managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(1), 0, true);
-        currentUnitNumber++;
-        UnitDeployTextUpdate(currentUnitNumber);
+        UnitCreat(1);
     }
 
     public void Nun2Button() //수녀 버튼
     {
-        managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(2), 0, true);
-        currentUnitNumber++;
-        UnitDeployTextUpdate(currentUnitNumber);
+        UnitCreat(2);
     } 
 
     public void Cowardly_KinghtButton() //갑옷 
     {
-        managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(3), 0, true);
-        currentUnitNumber++;
-        UnitDeployTextUpdate(currentUnitNumber);
+        UnitCreat(3);
     }
 
     public void TA001Buttton() //번개궁수
     {
-        managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(4), 0, true);
-        currentUnitNumber++;
-        UnitDeployTextUpdate(currentUnitNumber);
+        UnitCreat(4);
+    }
+
+    //unit  생성 & Unit 삭제에 대한 메서드 작성
+
+    void UnitCreat(uint num) //Unit 생성 
+    {
+        if (currentUnitNumber < maxUnitNumber)
+        {
+            managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(num), 0, true);
+            currentUnitNumber++;
+            UnitDeployTextUpdate(currentUnitNumber);
+        }
+        else
+        {
+            StartCoroutine(WarnningTextPrint());
+            Debug.Log("[Unit Deployment Manager]유닛 배치 최대입니다.");
+        }
+    }
+
+    IEnumerator WarnningTextPrint() //경고 문구
+    {
+        warnningObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        warnningObject.SetActive(false);
     }
 }
