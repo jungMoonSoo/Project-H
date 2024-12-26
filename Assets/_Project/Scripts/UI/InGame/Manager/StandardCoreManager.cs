ca0b156099fc2a495fe845999cd156eeb123ac6b
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-26 14:33:41 +0900

    클리어 UI 제작

--- Assets/_Project/Scripts/UI/InGame/Manager/StandardCoreManager.cs
+++ Assets/_Project/Scripts/UI/InGame/Manager/StandardCoreManager.cs
@@ -119,7 +119,7 @@ public class StandardCoreManager : Singleton<StandardCoreManager>
         timer.GetComponentInChildren<Text>().text = $"{minutes:D2} : {seconds:D2}";
     }
 
-    //Game (승리 / 패배 / Wave Clear) 관련 함수 
+    //Game (승리 / 패배 / Stage Clear) 관련 함수 
     public void GameVictory() //승리 문구 출력 메서드 
     {
         Debug.Log("[Standard Core Manager]게임에서 승리하셨습니다.");
@@ -127,6 +127,7 @@ public class StandardCoreManager : Singleton<StandardCoreManager>
         gameEnd.SetActive(true);
         gameEnd.transform.GetChild(0).gameObject.SetActive(true);
         gameEnd.transform.GetChild(1).gameObject.SetActive(false);
+        gameEnd.transform.GetChild(2).gameObject.SetActive(false);
     }
     public void GameDefeat() //패배 문구 출력 메서드
     {
@@ -135,6 +136,17 @@ public class StandardCoreManager : Singleton<StandardCoreManager>
         gameEnd.SetActive(true);
         gameEnd.transform.GetChild(0).gameObject.SetActive(false);
         gameEnd.transform.GetChild(1).gameObject.SetActive(true);
+        gameEnd.transform.GetChild(2).gameObject.SetActive(false);
+    }
+
+    public void StageClear() //웨이브 클리어 문구 출력 메서드
+    {
+        Debug.Log("[Standard Core Manager]해당 스테이지를 클리어 하였습니다.");
+
+        gameEnd.SetActive(true);
+        gameEnd.transform.GetChild(0).gameObject.SetActive(false);
+        gameEnd.transform.GetChild(1).gameObject.SetActive(false);
+        gameEnd.transform.GetChild(2).gameObject.SetActive(true);
     }
 
     //유닛 스킬 테스트용?
