57f3406cdfb88b255bb5cc60f31d5d76fc642b88
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-30 20:51:37 +0900

    Managers 수정 및 Phase 수정

--- Assets/_Project/Scripts/System/PhaseManager/PhaseState/ReadyPhaseState.cs
+++ Assets/_Project/Scripts/System/PhaseManager/PhaseState/ReadyPhaseState.cs
@@ -38,15 +38,6 @@ public class ReadyPhaseState : MonoBehaviour, IPhaseState
     bool nun2Spawn = false;
     bool cowardly_KinghtSpawn = false;
     bool ta001Spawn = false;
-
-    //Hold 관련 변수 
-    float pressStartTime;                  //누르기 시작한 시간
-    bool isPressing = false;               //누르는 상태 여부 
-    const float longPressThreshold = 0.5f; //길게 누르기 판정 시간 
-    GameObject targetUnit = null;          //눌린 타일 
-
-    private Vector2 initialTouchPosition; // 터치 시작 위치
-    private const float stationaryThreshold = 1.0f; // 이동 허용 범위 (픽셀 단위)
     #endregion
 
     public void OnEnter()
@@ -56,80 +47,14 @@ public class ReadyPhaseState : MonoBehaviour, IPhaseState
 
     public void OnUpdate()
     {
-        TouchInfo touch = TouchSystem.Instance.GetTouch(0);
-
-        switch (touch.phase)
-        {
-            case TouchPhase.Began:
-                if (touch.gameObject != null)
-                {
-                    targetUnit = touch.gameObject.transform.parent.parent.gameObject;
-                    initialTouchPosition = touch.pos;
-                    pressStartTime = Time.time;
-                    isPressing = true;
-                }
-                break;
-            case TouchPhase.Moved:// 터치가 고정되거나 약간 움직이는 경우
-                if (isPressing)
-                {
-                    float pressDuration = Time.time - pressStartTime;
-                    float distance = Vector2.Distance(initialTouchPosition, touch.pos);
-
-                    if (distance > stationaryThreshold) //Hold 취소
-                    {
-                        Debug.Log("[Ui Manager]터치가 움직였습니다. Hold가 취소되었습니다.");
-                        isPressing = false;
-                    }
-
-                    if (pressDuration >= longPressThreshold)
-                    {
-                        HandleLongPress(); // 길게 누르기 처리
-                        isPressing = false; // 한 번만 실행되도록 플래그 해제
-                    }
-                }
-                break;
-            case TouchPhase.Ended:
-            case TouchPhase.Canceled:
-                isPressing = false; // 터치가 끝나면 플래그 초기화
-                break;
-        }
     }
 
     public void OnExit()
     {
         unitDeploymentManger.SetActive(false);
     }
-    private void HandleLongPress()//길게 눌렸을 때 적용되는 UI
-    {
-        if (targetUnit.GetComponent<Unidad>() != null)
-        {
-            if (targetUnit.GetComponent<Unidad>().Owner == UnitType.Ally)
-            {
-                Debug.Log("[Standard Game Star tUI]아군 유닛이 선택되었습니다.");
-                allyInfo.gameObject.SetActive(true);
-                allyInfo.GetComponentInChildren<Text>().text = targetUnit.GetComponent<Unidad>().Status.name;
-            }
-            else if (targetUnit.GetComponent<Unidad>().Owner == UnitType.Enemy)
-            {
-                Debug.Log("[Standard Game Start UI]적군 유닛이 선택되었습니다.");
-                enemyInfo.gameObject.SetActive(true);
-                enemyInfo.GetComponentInChildren<Text>().text = targetUnit.GetComponent<Unidad>().Status.name;
-            }
-        }
-    }
 
     #region◇System 관련 메서드◇ 
-    public void CloseInfoWindow() //설명창 비활성화
-    {
-        if (allyInfo.activeSelf)
-        {
-            allyInfo.gameObject.SetActive(false);
-        }
-        else if (enemyInfo.activeSelf)
-        {
-            enemyInfo.gameObject.SetActive(false);
-        }
-    }
     public void BackWindowButton() //뒤로 가기 버튼
     {
         Debug.Log("[Ui Manager]뒤로가기 버튼을 눌렸습니다. ");
--- Assets/_Project/Scripts/System/PhaseManager/PhaseState/RunPhaseState.cs
+++ Assets/_Project/Scripts/System/PhaseManager/PhaseState/RunPhaseState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,6 +17,18 @@ public class RunPhaseState : MonoBehaviour, IPhaseState
     float timeInSeconds = 120f;     //초기 타이머 시간(한판에 걸리는 시간)
     bool isTimerRunning = false;    //타이머 실행 여부
 
+    //Hold 관련 변수 
+    float pressStartTime;                  //누르기 시작한 시간
+    bool isPressing = false;               //누르는 상태 여부 
+    const float longPressThreshold = 0.5f; //길게 누르기 판정 시간 
+    GameObject targetUnit = null;          //눌린 타일 
+
+    private Vector2 initialTouchPosition; // 터치 시작 위치
+    private const float stationaryThreshold = 1.0f; // 이동 허용 범위 (픽셀 단위)
+    
+    [Header("Information 연결")]
+    [SerializeField] GameObject allyInfo;
+    [SerializeField] GameObject enemyInfo;
     public void OnEnter()
     {
         runWindow.SetActive(true);
@@ -23,6 +36,54 @@ public class RunPhaseState : MonoBehaviour, IPhaseState
 
     public void OnUpdate()
     {
+        TouchInfo touch = TouchSystem.Instance.GetTouch(0);
+
+        switch (touch.phase)
+        {
+            case TouchPhase.Began:
+                if (touch.gameObject != null)
+                {
+                    targetUnit = touch.gameObject.transform.parent.parent.gameObject;
+                    initialTouchPosition = touch.pos;
+                    pressStartTime = Time.time;
+                    isPressing = true;
+                }
+                break;
+            case TouchPhase.Moved:// 터치가 고정되거나 약간 움직이는 경우
+                if (isPressing)
+                {
+                    float pressDuration = Time.time - pressStartTime;
+                    float distance = Vector2.Distance(initialTouchPosition, touch.pos);
+
+                    if (distance > stationaryThreshold) //Hold 취소
+                    {
+                        Debug.Log("[Ui Manager]터치가 움직였습니다. Hold가 취소되었습니다.");
+                        isPressing = false;
+                    }
+
+                    if (pressDuration >= longPressThreshold)
+                    {
+                        HandleLongPress(); // 길게 누르기 처리
+                        isPressing = false; // 한 번만 실행되도록 플래그 해제
+                    }
+                }
+                break;
+            case TouchPhase.Stationary:
+                if (isPressing)
+                {
+                    float pressDuration = Time.time - pressStartTime;
+                    if (pressDuration >= longPressThreshold)
+                    {
+                        HandleLongPress(); // 길게 누르기 처리
+                        isPressing = false; // 한 번만 실행되도록 플래그 해제
+                    }
+                }
+                break;
+            case TouchPhase.Ended:
+            case TouchPhase.Canceled:
+                isPressing = false; // 터치가 끝나면 플래그 초기화
+                break;
+        }
     }
 
     public void OnExit()
@@ -62,5 +123,34 @@ public class RunPhaseState : MonoBehaviour, IPhaseState
 
         // "MM:SS" 형식으로 텍스트 업데이트
         timer.GetComponentInChildren<Text>().text = $"{minutes:D2} : {seconds:D2}";
+    }
+    private void HandleLongPress()//길게 눌렸을 때 적용되는 UI
+    {
+        if (targetUnit.GetComponent<Unidad>() != null)
+        {
+            if (targetUnit.GetComponent<Unidad>().Owner == UnitType.Ally)
+            {
+                Debug.Log("[Standard Game Star tUI]아군 유닛이 선택되었습니다.");
+                allyInfo.gameObject.SetActive(true);
+                allyInfo.GetComponentInChildren<Text>().text = targetUnit.GetComponent<Unidad>().Status.name;
+            }
+            else if (targetUnit.GetComponent<Unidad>().Owner == UnitType.Enemy)
+            {
+                Debug.Log("[Standard Game Start UI]적군 유닛이 선택되었습니다.");
+                enemyInfo.gameObject.SetActive(true);
+                enemyInfo.GetComponentInChildren<Text>().text = targetUnit.GetComponent<Unidad>().Status.name;
+            }
+        }
+    }
+    public void CloseInfoWindow() //설명창 비활성화
+    {
+        if (allyInfo.activeSelf)
+        {
+            allyInfo.gameObject.SetActive(false);
+        }
+        else if (enemyInfo.activeSelf)
+        {
+            enemyInfo.gameObject.SetActive(false);
+        }
     }
 }
