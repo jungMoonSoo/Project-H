ecb99296cfd02368831748640c52bf956f509b8c
RFGEM <tkdenddlquf@naver.com>
2024-12-30 22:59:05 +0900

    원뿔 범위 탐색 시 좌표로 각도를 지정하도록 변경

--- Assets/_Project/Scripts/Skills/TargetingSystem/ArcTargetingSystem.cs
+++ Assets/_Project/Scripts/Skills/TargetingSystem/ArcTargetingSystem.cs
@@ -16,50 +16,49 @@ public class ArcTargetingSystem: ITargetingSystem
     {
         List<Unidad> targets = new(UnidadManager.Instance.unidades.Where(x => x.Owner == targetOwner));
 
+        float castedAngle = GetDirectionToAngle(castedPosition - casterPosition);
+        Vector2 range = new(Mathf.Repeat(castedAngle - rangeSize.y * 0.5f, 360), Mathf.Repeat(castedAngle + rangeSize.y * 0.5f, 360));
+        Vector2 areaSize = new(rangeSize.x, rangeSize.x);
+
         for (int i = targets.Count - 1; i >= 0; i--)
         {
             Vector2 targetPos = (Vector2)targets[i].transform.position + targets[i].unitCollider.center;
 
-            if (!CheckTargetInArea(casterPosition, rangeSize, targetPos, targets[i].unitCollider.Radius, castedPosition)) targets.Remove(targets[i]);
+            if (!CheckTargetInArea(casterPosition, areaSize, targetPos, targets[i].unitCollider.Radius, range)) targets.Remove(targets[i]);
         }
 
         return targets.ToArray();
     }
 
-    private bool CheckTargetInArea(Vector2 centerPos, Vector2 rangeSize, Vector2 targetPos, Vector2 targetSize, Vector2 angle)
+    private bool CheckTargetInArea(Vector2 areaPos, Vector2 areaSize, Vector2 targetPos, Vector2 targetSize, Vector2 range)
     {
-        if (VectorCalc.CalcEllipse(centerPos, targetPos, rangeSize, targetSize) > 1f) return false;
+        if (VectorCalc.CalcEllipse(areaPos, targetPos, areaSize, targetSize) > 1f) return false;
 
         foreach (Vector2 point in GetContactPoints(targetPos, targetSize))
         {
-            if (CheckDirectionInAngle(point - centerPos, angle)) return true;
+            if (CheckDirectionInAngle(GetDirectionToAngle(point - areaPos), range)) return true;
         }
 
-        if (CheckDirectionInAngle(targetPos - centerPos, angle)) return true;
+        if (CheckDirectionInAngle(GetDirectionToAngle(targetPos - areaPos), range)) return true;
 
         return false;
     }
 
-    private bool CheckDirectionInAngle(Vector2 direction, Vector2 angle)
+    private bool CheckDirectionInAngle(float angle, Vector2 range)
     {
-        float centerAngle = Mathf.Repeat(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, 360);
-        float normalizedStartCenter = Mathf.Repeat(angle.x, 360);
-        float normalizedEndCenter = Mathf.Repeat(angle.y, 360);
-
-        if (normalizedStartCenter <= normalizedEndCenter)
+        if (range.x <= range.y)
         {
-            if (centerAngle >= normalizedStartCenter && centerAngle <= normalizedEndCenter) return true;
+            if (angle >= range.x && angle <= range.y) return true;
         }
         else
         {
-            if (centerAngle >= normalizedStartCenter || centerAngle <= normalizedEndCenter) return true;
+            if (angle >= range.x || angle <= range.y) return true;
         }
 
         return false;
     }
 
-    private Vector2[] GetContactPoints(Vector2 centerB, Vector2 radiiB)
-    {
-        return new Vector2[] { centerB + new Vector2(-radiiB.x, 0), centerB + new Vector2(radiiB.x, 0), centerB + new Vector2(0, -radiiB.y), centerB + new Vector2(0, radiiB.y) };
-    }
+    private float GetDirectionToAngle(Vector2 direction) => Mathf.Repeat(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, 360);
+
+    private Vector2[] GetContactPoints(Vector2 pos, Vector2 size) => new Vector2[] { pos + new Vector2(-size.x, 0), pos + new Vector2(size.x, 0), pos + new Vector2(0, -size.y), pos + new Vector2(0, size.y) };
 }
