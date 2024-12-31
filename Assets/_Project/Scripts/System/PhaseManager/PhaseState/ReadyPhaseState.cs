636a7e7578e824280d8ffb579496e14fed28bc27
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-31 14:25:59 +0900

    스크립트 분리(UnitDeploymentButton)

--- Assets/_Project/Scripts/System/PhaseManager/PhaseState/ReadyPhaseState.cs
+++ Assets/_Project/Scripts/System/PhaseManager/PhaseState/ReadyPhaseState.cs
@@ -5,44 +5,16 @@ using UnityEngine.UI;
 
 public class ReadyPhaseState : MonoBehaviour, IPhaseState
 {
-    #region ◇변수◇
-    [Header("Camera 할당")]
-    [SerializeField] Camera mainCamera;
-
-    [Header("Information 연결")]
-    [SerializeField] GameObject allyInfo;
-    [SerializeField] GameObject enemyInfo;
-
     [Header("GameObject 연결")]
-    [SerializeField] GameObject unitDeploymentManger;
-    [SerializeField] GameObject warnningObject;
-    [SerializeField] Text stageNumber;
-    [SerializeField] Text unitNumber;
-
-    [Header("Unit 관리 메니져")]
-    [SerializeField] GameObject managers;
-
-    [Header("Ally Tile Handle")]
-    [SerializeField] TileHandle[] tileInfo;
+    [SerializeField] private GameObject unitDeploymentObject;
+    [SerializeField] private Text stageText;
 
     // 스테이지 정보 -를 기준으로 앞 뒤 숫자를 받아옴
-    int frontStageNumber = 0;
-    int backStageNumber = 0;
-
-    int currentUnitNumber = 1; //현재 필드에 있는 unit 수
-    int maxUnitNumber = 5;     //필드 최대 unit 수
-
-    //임시 버튼
-    bool blocksmithSpwan = false;
-    bool alchemySpawn = false;
-    bool nun2Spawn = false;
-    bool cowardly_KinghtSpawn = false;
-    bool ta001Spawn = false;
-    #endregion
-
+    private int frontStageNumber = 0;
+    private int backStageNumber = 0;
     public void OnEnter()
     {
-        unitDeploymentManger.SetActive(true);
+        unitDeploymentObject.SetActive(true);
     }
 
     public void OnUpdate()
@@ -51,141 +23,19 @@ public class ReadyPhaseState : MonoBehaviour, IPhaseState
 
     public void OnExit()
     {
-        unitDeploymentManger.SetActive(false);
+        unitDeploymentObject.SetActive(false);
     }
 
-    #region◇System 관련 메서드◇ 
     public void BackWindowButton() //뒤로 가기 버튼
     {
         Debug.Log("[Ui Manager]뒤로가기 버튼을 눌렸습니다. ");
     }
+
     public void StageTextUpdate(int front, int back) //Stage 정보 업데이트 함수 
     {
         frontStageNumber = front;
         backStageNumber = back;
 
-        stageNumber.text = $"{frontStageNumber} - {backStageNumber}";
+        stageText.text = $"{frontStageNumber} - {backStageNumber}";
     }
-    public void UnitDeployTextUpdate(int num) //배치 수 업데이트 함수 
-    {
-        currentUnitNumber = num;
-        unitNumber.text = $"배치수 ({currentUnitNumber} / {maxUnitNumber})";
-    }
-
-    //코루틴 함수 
-    IEnumerator WarningTextPrint() //경고 문구
-    {
-        warnningObject.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        warnningObject.SetActive(false);
-    }
-    #endregion
-
-    #region◇Unidad 배치 메서드◇ 
-    public void BlocksmithButton() //대장장이 버튼 
-    {
-        if (!blocksmithSpwan)
-        {
-            CreateUnit(0);
-            blocksmithSpwan = true;
-        }
-        else
-        {
-            DestroyUnit(0);
-            blocksmithSpwan = false;
-
-        }
-    }
-
-    public void AlchemyButton()//물약이 버튼
-    {
-        if (!alchemySpawn)
-        {
-            CreateUnit(1);
-            alchemySpawn = true;
-        }
-        else
-        {
-            DestroyUnit(1);
-            alchemySpawn = false;
-        }
-    }
-
-    public void Nun2Button() //수녀 버튼
-    {
-        if (!nun2Spawn)
-        {
-            CreateUnit(2);
-            nun2Spawn = true;
-        }
-        else
-        {
-            DestroyUnit(2);
-            nun2Spawn = false;
-        }
-    }
-
-    public void Cowardly_KinghtButton() //갑옷 
-    {
-        if (!cowardly_KinghtSpawn)
-        {
-            CreateUnit(3);
-            cowardly_KinghtSpawn = true;
-        }
-        else
-        {
-            DestroyUnit(3);
-            cowardly_KinghtSpawn = false;
-        }
-    }
-
-    public void TA001Buttton() //번개궁수
-    {
-        if (!ta001Spawn)
-        {
-            CreateUnit(4);
-            ta001Spawn = true;
-        }
-        else
-        {
-            DestroyUnit(4);
-            ta001Spawn = false;
-        }
-    }
-
-    //unit  생성 & Unit 삭제에 대한 메서드 작성
-
-    void CreateUnit(uint num) //Unit 생성 
-    {
-        if (currentUnitNumber < maxUnitNumber)
-        {
-            managers.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(num), 0, true);
-            currentUnitNumber++;
-            UnitDeployTextUpdate(currentUnitNumber);
-        }
-        else
-        {
-            StartCoroutine(WarningTextPrint());
-            Debug.Log("[Ui Manager]유닛 배치 최대입니다.");
-        }
-    }
-
-    void DestroyUnit(uint num) //Unit 삭제 => 삭제 메세드 
-    {
-        for (int i = 0; i < tileInfo.Length; i++)
-        {
-            if (tileInfo[i].Unit != null)
-            {
-                if (tileInfo[i].Unit.Status == UnidadManager.Instance.GetStatus(num))
-                {
-                    currentUnitNumber -= 1;
-                    Destroy(tileInfo[i].Unit.gameObject);
-                    tileInfo[i].RemoveUnit();
-                    Debug.Log("[Ui Manager]해당 유닛을 제거하였습니다.");
-                    break;
-                }
-            }
-        }
-    }
-    #endregion
 }
---
+++ Assets/_Project/Scripts/UI/InGame/UnitDeploymentButton.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitDeploymentButton : MonoBehaviour
+{
+    [Header("GameObject 연결")]
+    [SerializeField] GameObject warnningObject;
+    [SerializeField] Text unitNumber;
+
+    [Header("Unit 관리 메니져")]
+    [SerializeField] GameObject unitManagerObject;
+
+    [Header("Ally Tile Handle")]
+    [SerializeField] TileHandle[] tileInfo;
+
+    int currentUnitNumber = 1; //현재 필드에 있는 unit 수
+    int maxUnitNumber = 5;     //필드 최대 unit 수
+
+    bool blocksmithSpwan = false;
+    bool alchemySpawn = false;
+    bool nun2Spawn = false;
+    bool cowardly_KinghtSpawn = false;
+    bool ta001Spawn = false;
+
+    public void UnitDeployTextUpdate(int num) //배치 수 업데이트 함수 
+    {
+        currentUnitNumber = num;
+        unitNumber.text = $"배치수 ({currentUnitNumber} / {maxUnitNumber})";
+    }
+    IEnumerator WarningTextPrint() //경고 문구
+    {
+        warnningObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        warnningObject.SetActive(false);
+    }
+
+    #region◇Unidad 배치 메서드◇ 
+    public void BlocksmithButton() //대장장이 버튼 
+    {
+        if (!blocksmithSpwan)
+        {
+            CreateUnit(0);
+            blocksmithSpwan = true;
+        }
+        else
+        {
+            DestroyUnit(0);
+            blocksmithSpwan = false;
+
+        }
+    }
+
+    public void AlchemyButton()//물약이 버튼
+    {
+        if (!alchemySpawn)
+        {
+            CreateUnit(1);
+            alchemySpawn = true;
+        }
+        else
+        {
+            DestroyUnit(1);
+            alchemySpawn = false;
+        }
+    }
+
+    public void Nun2Button() //수녀 버튼
+    {
+        if (!nun2Spawn)
+        {
+            CreateUnit(2);
+            nun2Spawn = true;
+        }
+        else
+        {
+            DestroyUnit(2);
+            nun2Spawn = false;
+        }
+    }
+
+    public void Cowardly_KinghtButton() //갑옷 
+    {
+        if (!cowardly_KinghtSpawn)
+        {
+            CreateUnit(3);
+            cowardly_KinghtSpawn = true;
+        }
+        else
+        {
+            DestroyUnit(3);
+            cowardly_KinghtSpawn = false;
+        }
+    }
+
+    public void TA001Buttton() //번개궁수
+    {
+        if (!ta001Spawn)
+        {
+            CreateUnit(4);
+            ta001Spawn = true;
+        }
+        else
+        {
+            DestroyUnit(4);
+            ta001Spawn = false;
+        }
+    }
+
+    //unit  생성 & Unit 삭제에 대한 메서드 작성
+
+    void CreateUnit(uint num) //Unit 생성 
+    {
+        if (currentUnitNumber < maxUnitNumber)
+        {
+            unitManagerObject.GetComponent<UnidadSpawnManager>().Spawn(UnidadManager.Instance.GetStatus(num), 0, true);
+            currentUnitNumber++;
+            UnitDeployTextUpdate(currentUnitNumber);
+        }
+        else
+        {
+            StartCoroutine(WarningTextPrint());
+            Debug.Log("[Ui Manager]유닛 배치 최대입니다.");
+        }
+    }
+
+    void DestroyUnit(uint num) //Unit 삭제 => 삭제 메세드 
+    {
+        for (int i = 0; i < tileInfo.Length; i++)
+        {
+            if (tileInfo[i].Unit != null)
+            {
+                if (tileInfo[i].Unit.Status == UnidadManager.Instance.GetStatus(num))
+                {
+                    currentUnitNumber -= 1;
+                    Destroy(tileInfo[i].Unit.gameObject);
+                    tileInfo[i].RemoveUnit();
+                    Debug.Log("[Ui Manager]해당 유닛을 제거하였습니다.");
+                    break;
+                }
+            }
+        }
+    }
+    #endregion
+}
