2684f03e48b4ffcd2de56b74836a94247e5cffc7
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-12-31 15:59:17 +0900

    class명 수정

--- Assets/_Project/Scripts/UI/InGame/AllyUnitDeployment.cs
+++ Assets/_Project/Scripts/UI/InGame/AllyUnitDeployment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UnitDeploymentButton : MonoBehaviour
+public class AllyUnitDeploymen : MonoBehaviour
 {
     [Header("GameObject 연결")]
     [SerializeField] private GameObject maxWarnningObject;
