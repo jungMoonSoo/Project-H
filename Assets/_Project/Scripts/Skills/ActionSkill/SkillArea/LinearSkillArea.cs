263c7168b3743342d629123acfcc1f275e3aac6f
sirosi <zaq15478@naver.com>
2025-02-06 14:47:33 +0900

    직선형 스킬 구조 변경

--- Assets/_Project/Scripts/Skills/ActionSkill/SkillArea/LinearSkillArea.cs
+++ Assets/_Project/Scripts/Skills/ActionSkill/SkillArea/LinearSkillArea.cs
@@ -8,7 +8,7 @@ public class LinearSkillArea: ISkillArea
         transform.position = casterPosition;
 
         float angle = VectorCalc.CalcRotation(castedPosition - casterPosition);
-        transform.eulerAngles = new Vector3(0, 0, angle);
+        transform.eulerAngles = new Vector3(0, angle, 0);
 
         // TODO: 실제 스킬이 날아갈 위치 정해야 함.
         return castedPosition;
