410fc7f306ee545495fc43afb14853ec6d4362dd
sirosi <zaq15478@naver.com>
2025-02-20 16:00:39 +0900

    난수발생 방식 변경

--- Assets/_Project/Scripts/Utilities/RandomModule.cs
+++ Assets/_Project/Scripts/Utilities/RandomModule.cs
@@ -1,11 +1,20 @@
+using AmplifyShaderEditor;
 using System;
+using System.Security.Cryptography;
+using UnityEngine;
 
 public static class RandomModule
 {
-    public static int GetRandom(int max)
-    {
-        int microsecond = Convert.ToInt32(DateTime.Now.ToString("ffffff"));
+    private static readonly RandomNumberGenerator generator = RandomNumberGenerator.Create();
 
-        return new Random(microsecond).Next(max);
+    public static long GetSecurityRandom(long min, long max)
+    {
+        byte[] bytes = new byte[8];
+        generator.GetBytes(bytes);
+        
+        long rValue = BitConverter.ToInt64(bytes);
+        long aValue = rValue == long.MinValue ? 0 : Math.Abs(rValue);
+        
+        return aValue % (max - min) + min;
     }
 }
