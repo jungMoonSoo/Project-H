da8c3c680ded0b824944a5a0cdb2cd7aa7408008
sirosi <zaq15478@naver.com>
2025-03-06 16:10:01 +0900

    임시 옵션창 및 볼륨 슬라이더 구현

--- Assets/_Project/Scripts/System/OptionManager.cs
+++ Assets/_Project/Scripts/System/OptionManager.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionManager : MonoBehaviour
+public class OptionManager : Singleton<OptionManager>
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject optionUiObject;
+    
+    [SerializeField] private Slider sldMasterVolume;
+    [SerializeField] private Slider sldMusicVolume;
+    [SerializeField] private Slider sldEffectVolume;
+    [SerializeField] private Slider sldVoiceVolume;
+
+
+    [NonSerialized] public float MasterVolume;
+    [NonSerialized] public float MusicVolume;
+    [NonSerialized] public float EffectVolume;
+    [NonSerialized] public float VoiceVolume;
+
+
     void Start()
     {
-        
+        Init();
+    }
+    
+
+    public void Init()
+    {
+        sldMasterVolume.onValueChanged.AddListener((value) => MasterVolume = value);
+        sldMusicVolume.onValueChanged.AddListener((value) => MusicVolume = value);
+        sldEffectVolume.onValueChanged.AddListener((value) => EffectVolume = value);
+        sldVoiceVolume.onValueChanged.AddListener((value) => VoiceVolume = value);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FirstSetting()
     {
         
     }
+    
+    public void OpenUi() => optionUiObject.SetActive(true);
+    public void CloseUi() => optionUiObject.SetActive(false);
+}
+
+public enum VolumeType
+{
+    Master,
+    Music,
+    Effect,
+    Voice
 }
