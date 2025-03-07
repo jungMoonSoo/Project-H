5c23356860db689c22b5424756d69e8170fdc294
RFGEM <tkdenddlquf@naver.com>
2025-03-07 21:31:44 +0900

    오브젝트가 파괴된 경우 Look 시스템을 해제하도록 설정

--- Assets/_Project/Scripts/UI/InGame/System/LookCameraSystem.cs
+++ Assets/_Project/Scripts/UI/InGame/System/LookCameraSystem.cs
@@ -4,10 +4,14 @@ public class LookCameraSystem : MonoBehaviour
 {
     private void Start()
     {
-        LookController.Instance.actions -= Look;
         LookController.Instance.actions += Look;
 
         Look(LookController.Instance.Rotation);
+    }
+
+    private void OnDestroy()
+    {
+        LookController.Instance.actions -= Look;
     }
 
     private void Look(Vector3 rotation) => transform.Rotate(rotation);
